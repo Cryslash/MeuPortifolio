48f0daafeffa7fb832a57ea57fa5d25a2bbef9b1
Crystofher Lins <crystofher_lins@outlook.com>
2021-08-01 03:01:27 -0300

    home responsiveness

--- frontend/main.js
+++ frontend/main.js
@@ -2,3 +2,21 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import './assets/css/style.css';
+
+function menu() {
+
+    let show = true;
+
+    const rowmenu = document.querySelector('.rowmenu');
+    const toggle = document.querySelector('.menu-toggle');
+    
+    toggle.addEventListener('click', () => {
+        
+        document.body.style.overflow = show ? 'hidden' : 'initial';
+
+        rowmenu.classList.toggle('on', show);        
+        show = !show;
+    })
+
+};
+menu();
